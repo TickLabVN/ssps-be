456d2404713c51e2328b594d67349cb45e048d5b
quannhg <quan.nguyenhg12@gmail.com>
2023-12-03 18:14:12 +0700

    feat(configuration): implement serving fee configuration and missing get configuration APIs
    This feature includes the addition of serving fee configuration and handlers, as well as updates to
    other handlers to maintain persistence between data. The retrieval of serving fee, coin per page,
    coin per semester, dollar to coin ratio, and max file size APIs have also been updated.

--- src/constants/PrintingRequest.ts
+++ src/constants/PrintingRequest.ts
@@ -16,3 +16,4 @@ export const DEFAULT_ACCEPTED_EXTENSION = ['pdf'];
 export const DEFAULT_COIN_PER_PAGE = 2;
 export const DEFAULT_COIN_PER_SEM = 100;
 export const DEFAULT_MAX_FILE_SIZE = 100 * 1024 * 1024;
+export const DEFAULT_SERVICE_FEE = 5;
